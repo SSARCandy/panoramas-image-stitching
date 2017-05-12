e5e832717963c9de0634da723f0a292714a28cb7
ssarcandy <ssarcandy@gmail.com>
2017-05-13 00:29:52 +0800

    better crop function

--- src/stitch.py
+++ src/stitch.py
@@ -119,13 +119,15 @@ def crop(img):
     _, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1, 255, cv2.THRESH_BINARY)
     upper, lower = [-1, -1]
 
+    black_pixel_num_threshold = img.shape[1]//100
+
     for y in range(thresh.shape[0]):
-        if len(np.where(thresh[y] == 0)[0]) == 0:
+        if len(np.where(thresh[y] == 0)[0]) < black_pixel_num_threshold:
             upper = y
             break
         
     for y in range(thresh.shape[0]-1, 0, -1):
-        if len(np.where(thresh[y] == 0)[0]) == 0:
+        if len(np.where(thresh[y] == 0)[0]) < black_pixel_num_threshold:
             lower = y
             break
 
