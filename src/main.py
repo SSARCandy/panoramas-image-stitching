f2ccd7de6746df3ea8bb7ab6d97907afaa72e0fe
Shu Hsuan Hsu <ssarcandy@cml25.csie.ntu.edu.tw>
2017-05-13 22:32:23 +0800

    cache previous img features

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 import cv2
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import multiprocessing as mp
 import feature
 import utils
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
     img_list, focal_length = utils.parse(input_dirname)
     
-    # img_list = img_list[:3]
+    # img_list = img_list[:2]
 
     print('Warp images to cylinder')
     cylinder_img_list = pool.starmap(utils.cylindrical_projection, [(img_list[i], focal_length[i]) for i in range(len(img_list))])
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     stitched_image = cylinder_img_list[0].copy()
 
     shifts = [[0, 0]]
+    cache_feature = [[], []]
 
     # add first img for end to end align
     #cylinder_img_list += [stitched_image]
@@ -46,14 +47,18 @@ if __name__ == '__main__':
         img2 = cylinder_img_list[i]
 
         print(' - Find features in previous img .... ', end='', flush=True)
-        corner_response1 = feature.harris_corner(img1, pool)
-        descriptors1, position1 = feature.extract_description(img1, corner_response1, kernel=5, threshold=0.05)
+        descriptors1, position1 = cache_feature
+        if len(descriptors1) == 0:
+            corner_response1 = feature.harris_corner(img1, pool)
+            descriptors1, position1 = feature.extract_description(img1, corner_response1, kernel=5, threshold=0.01)
         print(str(len(descriptors1))+' features extracted.')
 
         print(' - Find features in img_'+str(i+1)+' .... ', end='', flush=True)
         corner_response2 = feature.harris_corner(img2, pool)
-        descriptors2, position2 = feature.extract_description(img2, corner_response2, kernel=5, threshold=0.05)
+        descriptors2, position2 = feature.extract_description(img2, corner_response2, kernel=5, threshold=0.01)
         print(str(len(descriptors2))+' features extracted.')
+
+        cache_feature = [descriptors2, position2]
 
         if False:
             cv2.imshow('cr1', corner_response1)
